c1751a29dc0a589388ba47e499ce54cc21ef38b7
leosgarbi <lsgarbi1@gmail.com>
2024-04-24 17:00:34 -0300

    feat: list content structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { PlusCircle } from '@phosphor-icons/react';
+import { Trash } from '@phosphor-icons/react/dist/ssr';
 import styles from './App.module.css';
 import { Button } from './components/Button';
 import { Header } from './components/Header';
@@ -11,7 +12,7 @@ export function App() {
       <Header />
 
       <section className={styles.content}>
-        <div className={styles.taskInfoContainer}>
+        <div className={styles.container}>
           <Input />
           <Button>
             Click
@@ -20,7 +21,46 @@ export function App() {
         </div>
 
         <div className={styles.tasksList}>
-          <div>Class list</div>
+          <header className={styles.headerContent}>
+            <aside>
+              Tarefas criadas <span>0</span>
+            </aside>
+            <aside>
+              Concluídas <span>2 de 5</span>
+            </aside>
+          </header>
+
+          {/* <div className={styles.dividerLine} /> */}
+
+          <div className={styles.taskListWrapper}>
+            <div className={styles.taskListContent}>
+              <input type='checkbox' />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+              <Trash color={'var(--gray-300)'} />
+            </div>
+          </div>
+          <div className={styles.taskListWrapper}>
+            <div className={styles.taskListContent}>
+              <input type='checkbox' />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
+                perferendis voluptatem iure ea pariatur consectetur?
+              </p>
+              <Trash color={'var(--gray-300)'} />
+            </div>
+          </div>
+          <div className={styles.taskListWrapper}>
+            <div className={styles.taskListContent}>
+              <input type='checkbox' />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+                reprehenderit voluptate nemo ullam facere mollitia. Sint officia
+                delectus deleniti laboriosam blanditiis natus porro?
+                Perspiciatis accusantium officiis ad in at ex? Est, rerum.
+              </p>
+              <Trash color={'var(--gray-300)'} />
+            </div>
+          </div>
         </div>
       </section>
     </main>
