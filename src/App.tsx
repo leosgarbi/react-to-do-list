9091ad40022c1312a38d4278226d1b47ddcff792
leosgarbi <lsgarbi1@gmail.com>
2024-04-24 12:04:38 -0300

    feat: add input and button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { PlusCircle } from '@phosphor-icons/react';
 import styles from './App.module.css';
+import { Button } from './components/Button';
 import { Header } from './components/Header';
+import { Input } from './components/Input';
 import './global.css';
 
 export function App() {
@@ -8,7 +11,17 @@ export function App() {
       <Header />
 
       <section className={styles.content}>
-        <div></div>
+        <div className={styles.taskInfoContainer}>
+          <Input />
+          <Button>
+            Click
+            <PlusCircle size={16} color='#f2f2f2' weight='bold' />
+          </Button>
+        </div>
+
+        <div className={styles.tasksList}>
+          <div>Class list</div>
+        </div>
       </section>
     </main>
   );
